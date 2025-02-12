556be0db211e1f993ec15565f35f5b69e57c72ce
Yafet Yosef <japhetkumalo23@gmail.com>
2025-02-12 11:20:06 +0300

    Fixed the bill name issue with bill card

--- src/components/BillCard.tsx
+++ src/components/BillCard.tsx
@@ -1,62 +1,79 @@
-import { useContext, useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import "../App.css";
-import { useNavigate } from "react-router-dom";
-import userContext from "../context/UserContext";
+import { Link } from "react-router-dom";
+// import userContext from "../context/UserContext";
 interface DatatoPass {
-  bill_name:string;
-  price:number;
-  createdDate:string;
-  id:string
+  bill_name: string;
+  price: number;
+  createdDate: string;
+  id: string;
+  status: string;
+  user_exchange: string;
 }
-const BillCard: React.FC<DatatoPass> = ({  bill_name, price, createdDate, id }) => {
+const BillCard: React.FC<DatatoPass> = ({
+  bill_name,
+  price,
+  createdDate,
+  id,
+  status,
+  user_exchange,
+}) => {
   const date = new Date(createdDate);
-  const options: Intl.DateTimeFormatOptions = {month:"short", day:'numeric', year:"numeric"}
-  const formattedDate=date.toLocaleString("en-US",options);
-  const context = useContext(userContext);
-  
+  const options: Intl.DateTimeFormatOptions = {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  };
+  const formattedDate = date.toLocaleString("en-US", options);
+
   const ref = useRef<HTMLElement>(null);
-  const navigate = useNavigate();
-  useEffect(() => {
-    
-  }, []);
+
   const btn = () => {
     console.log(ref.current?.classList);
   };
+  const [strings, setstrings] = useState<string>();
+  useEffect(() => {
+    if (status === "PENDING") {
+      setstrings("bg-yellow-200 text-yellow-700 p-2");
+    } else if (status === "PAID") {
+      setstrings("bg-green-200 text-green-700 p-2");
+    } else if (status === "UNPAID") {
+      setstrings("bg-red-200 text-red-700 p-2");
+    }
+  }, [status]);
   return (
-    <div
-      className="rounded-md w-[100%] bill-card md:w-[370px]"
-      onClick={() => {
-      navigate('/payment',{state:{bid:id},})
-      }}
-    >
-      <div>
-        <span>Bill name:</span>
-        <span>{bill_name}</span>
+    <Link to={`/payment/${id}`}>
+      <div className="rounded-md w-[100%] bill-card md:w-[370px]">
+        <div>
+          <span>Bill name:</span>
+          <span>{bill_name}</span>
+        </div>
+        <div>
+          <span>Date: </span>
+          <span>{formattedDate}</span>
+        </div>
+        <div>
+          <span>Price: </span>
+          <span>{price}</span>
+        </div>
+        <div>
+          <span>Status: </span>
+          <span
+            className={strings}
+            ref={ref}
+            onClick={() => {
+              btn();
+            }}
+          >
+            {status}
+          </span>
+        </div>
+        <div>
+          <span>Exchange: </span>
+          <span>{user_exchange}</span>
+        </div>
       </div>
-      <div>
-        <span>Date: </span>
-        <span>{formattedDate}</span>
-      </div>
-      <div>
-        <span>Price: </span>
-        <span>{price}</span>
-      </div>
-      <div>
-        <span>Status: </span>
-        <span className="text-red-600 bg-red-200 font-semibold p-2"
-          ref={ref}
-          onClick={() => {
-            btn();
-          }}
-        >
-          UNPAID
-        </span>
-      </div>
-      <div>
-        <span>Exchange: </span>
-        <span>N/A</span>
-      </div>
-    </div>
+    </Link>
   );
 };
 
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,70 +1,74 @@
 import { DollarSign } from "lucide-react";
 import "../App.css";
 import BillCard from "../components/BillCard";
-import { useContext, useEffect, useState } from "react";
-import userContext from "../context/UserContext";
+import { useEffect, useState } from "react";
+// import userContext from "../context/UserContext";
 import { supabase } from "../createClient";
 const Dashboard = () => {
-  const context = useContext(userContext);
-  const id = localStorage.getItem('user-id')
+  // const context = useContext(userContext);
+  const id = localStorage.getItem("user-id");
 
   console.log("Dashboard-page: ", id);
   interface Bill {
-    id:string;
-    created_at:string;
-    price:number;
-    bill_name:string;
+    id: string;
+    created_at: string;
+    price: number;
+    bill_name: string;
   }
   interface BillWithStatus {
-    bid:string;
-    uid:string;
-    transaction_no:string;
-    user_exchange:string;
-    created_at:string;
-    status:string;
+    bid: string;
+    uid: string;
+    transaction_no: string;
+    user_exchange: string;
+    created_at: string;
+    status: string;
+    bill_name:string
   }
-  const [bills,setbills] = useState<Bill[]>([
-   { bill_name:"",
-    id:"",
-    price:0,
-    created_at:""
-  }
+  const [bills, setbills] = useState<Bill[]>([
+    { bill_name: "", id: "", price: 0, created_at: "" },
   ]);
-  const[billWithStatus,setbillWithStatus] = useState<BillWithStatus[]>([
+  const [billWithStatus, setbillWithStatus] = useState<BillWithStatus[]>([
     {
-      bid:"",
-      status:"",
-      uid:"",
-      transaction_no:"",
-      created_at:"",
-      user_exchange:""
-    }
-  ])
+      bid: "",
+      status: "",
+      uid: "",
+      transaction_no: "",
+      created_at: "",
+      user_exchange: "",
+      bill_name:""
+    },
+  ]);
+
+
+  // console.log("Bill Id's pls: ",billId);
+
   // localStorage.setItem('user-name',context?.user?.user_name as string)
   // localStorage.setItem('user-id',context?.user?.id as string)
-  
-  const userName = localStorage.getItem('user-name')
-  useEffect(() => {
-const fetchBill = async () => {
-  // const id = localStorage.getItem('user-id')
 
-  const {data} = await supabase.from('bill').select('*')
-  if(data) 
-    setbills(data);
- 
-  const billStatusData = await supabase.from("bill_reference").select("*").eq('uid',id);
-  if(billStatusData.error) {
-    console.log("Bill Error: ",billStatusData.error);
-    
-  }
-  if(billStatusData.data)
-    setbillWithStatus(billStatusData.data);
-  
-  console.log("Bill: ",data);
-  console.log("BillStatus Data: ",billStatusData.data);
-  
-}
-fetchBill()
+  const userName = localStorage.getItem("user-name");
+  useEffect(() => {
+
+
+    const unPaidBill = async () => {
+      const { data } = await supabase.rpc("get_bills_not_in_reference", {
+        input_uid: id,
+      });
+      console.log("Unpaid Billlo: ", data);
+      setbills(data);
+      const billStatusData = await supabase.rpc("get_bill_reference_details", {
+        input_uid:id
+      })
+
+      if (billStatusData.error) {
+        console.log("Bill Error: ", billStatusData.error);
+      }
+      if (billStatusData.data) {
+        setbillWithStatus(billStatusData.data);
+        console.log("Wechew good: ",billStatusData.data)
+      }
+    };
+    // billIdFetch();
+    unPaidBill();
   }, []);
   return (
     <>
@@ -82,42 +86,58 @@ fetchBill()
         <div className=" md:flex">
           <div className="bg-white h-20 w-[85%] mx-auto my-0 flex justify-between items-center rounded-lg  mt-3 md:w-[300px] h-32 text-2xl">
             <div className="flex flex-col bg-white ml-5">
-              <span className="bg-white font-semibold text-3xl">5 </span>
-              <span className="bg-white">Paid fees</span>
+              <span className="bg-white font-semibold text-3xl">
+                {billWithStatus.length}{" "}
+              </span>
+              <span className="bg-white"> Pending & Paid fees</span>
             </div>
 
             <DollarSign className="bg-green-200 text-green-700 mr-5 w-10 h-10 rounded-full"></DollarSign>
           </div>
           <div className="bg-white h-20 w-[85%] mx-auto my-0 flex justify-between items-center rounded-lg  mt-3 md:w-[300px] h-32 text-2xl">
             <div className="flex flex-col bg-white ml-5">
-              <span className="bg-white font-semibold text-3xl">2 </span>
+              <span className="bg-white font-semibold text-3xl">
+                {bills.length}
+              </span>
               <span className="bg-white">Unpaid fees</span>
             </div>
 
             <DollarSign className="bg-red-200 text-red-700 mr-5 w-10 h-10 rounded-full"></DollarSign>
           </div>
-          <div className="bg-white h-20 w-[85%] mx-auto my-0 flex justify-between items-center rounded-lg  mt-3 md:w-[300px] h-32 text-2xl">
-            <div className="flex flex-col bg-white ml-5">
-              <span className="bg-white text-3xl font-semibold">2 </span>
-              <span className="bg-white">Pending fees</span>
-            </div>
-
-            <DollarSign className="bg-yellow-200 text-yellow-700 mr-5 w-10 h-10 rounded-full"></DollarSign>
-          </div>
         </div>
         <div className="flex flex-wrap gap-5 w-[90%] mx-auto my-0 justify-center  mt-5 md:w-[85%] gap-10 p-1 ">
+          {bills.map((b, i) => {
+            return (
+              <BillCard
+                key={i}
+                bill_name={b.bill_name}
+                price={b.price}
+                createdDate={b.created_at}
+                id={b.id}
+                status="UNPAID"
+                user_exchange=""
+              />
+            );
+          })}
+          <div>
+            <h2 className="block">Pending & Paid bills</h2>
+          </div>
+
+          <br></br>
+          {billWithStatus.map( (b, i) => {
         
-          {
-            bills.map((b) => {
-              return <BillCard bill_name={b.bill_name} price={b.price} createdDate={b.created_at} id={b.id} status="UNPAID"/>
-            })
-          }
-          <p>Pending & Paid bills</p>
-          {
-            billWithStatus.map((b,i) => {
-              return <BillCard bill_name={b.bid} id={b.bid} createdDate={b.created_at} price={125} key={i} status={b.status} />
-            })
-          }
+            return (
+              <BillCard
+                bill_name={b.bill_name}
+                id={b.bid}
+                createdDate={b.created_at}
+                price={125}
+                key={i}
+                status={b.status}
+                user_exchange={b.user_exchange}
+              />
+            );
+          })}
         </div>
       </section>
     </>
